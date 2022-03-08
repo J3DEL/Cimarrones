ab85700164ad403b202ab9ce3ef2608de2ae17f8
J3DEL <jairabeldelgado@gmail.com>
2022-03-08 16:04:08 -0500

    Eventos JavaScript

--- js/eventos.js
+++ js/eventos.js
@@ -1,26 +1,85 @@
+// Función para almacenar los datos del evento
 
 function evento(){
     
     let nombreEv = prompt('Ingrese el nombre del evento');
-    let fechaEv = prompt('Ingrese la fecha (formato: dd/mm/aaaa)');
     let direccionEv = prompt('Ingrese la direccion');
     let horaEv = prompt('Ingrese la hora (formato: hh:mm 24hrs)');
     
-    document.getElementById('nombreEvento').innerHTML=''+ nombreEv;
-    document.getElementById('fechaEvento').innerHTML=''+ fechaEv;
-    document.getElementById('direccionEvento').innerHTML=''+ direccionEv;
+    // Repetir hasta que la fecha es correcta
+    var d = 0; var m = 0; var a = 0;
+    while (a < 2022 || m > 12  && m <=2  || d > 31) {
+
+        var fechaEv = prompt('Ingrese la fecha (formato: dd/mm/aaaa)');
+    
+         d = Number( fechaEv.substring(0,2));
+         m = Number( fechaEv.substring(3,5));
+         a = Number( fechaEv.substring(6,10));
+        
+        if (a < 2022 || m > 12  || d > 31) {
+    
+            alert('Usted ha ingresado una fecha incorrecta');
+
+        } 
+        else if (a == 2022 && m == 1) {
+
+            alert('Su fecha contiene el mes de Enero de 2022, es una fecha pasada');
+
+        } 
+        else if (a == 2022 && m == 2) {
+            alert('Su fecha contiene el mes de Febrero de 2022, es una fecha pasada');
+        }
+         else{
+
+        }
+        
+        switch (m){
+    
+            // Meses que tienen 31 días
+    
+            case  3 || 5 || 7 || 9 || 10 || 11 || 12:
+                
+                if(d>=1 && d<=31){
+                    //si es verdadero no hacer nada
+             }else{
+                    //si es falso mandar alerta de error
+                alert('El mes que eligió no puede tener mas de 31 días');
+                }
+            break;
+    
+                // Meses que tienen 30 días
+    
+            case 4 || 6 || 8 || 11:
+        
+                if(d>=1 && d<=30){
+    
+                }else{
+            
+                    alert('El mes que eligió no puede tener mas de 30 días');
+                }
+                break;
+        
+                default:     
+        }
+    }
+
+    alert('¡Tu evento ha sido agregado con éxito!');
+
+    document.getElementById('nombreEvento').innerHTML='' + nombreEv;
+    document.getElementById('fechaEvento').innerHTML='' + fechaEv;
+    document.getElementById('direccionEvento').innerHTML='' + direccionEv;
     document.getElementById('horaEvento').innerHTML=''+ horaEv;
-
-    alert('¡Tu evento ha sido agregado con éxito!')
-}
-
-function mostrarEvento() {
-
-    document.getElementById('contenedor-evento').style.display = 'block';
+    
 }
 
 let ejecutarEvento = document.getElementById('botonjs');
 ejecutarEvento.addEventListener('click', evento);
+
+// Función para mostrar el evento que se ingresó
+function mostrarEvento() {
+
+    document.getElementById('contenedor-evento').style.display = 'block';
+}
 
 let ejecutarEvento2 = document.getElementById('botonjs');
 ejecutarEvento2.addEventListener('click', mostrarEvento);
