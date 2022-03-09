350417bd828f638d33c722a5e73f30fcc18ee8c1
J3DEL <jairabeldelgado@gmail.com>
2022-03-09 15:04:15 -0500

    update eventos

--- js/eventos.js
+++ js/eventos.js
@@ -4,34 +4,66 @@ function evento(){
     
     let nombreEv = prompt('Ingrese el nombre del evento');
     let direccionEv = prompt('Ingrese la direccion');
-    let horaEv = prompt('Ingrese la hora (formato: hh:mm 24hrs)');
+
+    // Repetir hasta que la hora sea correcta
+    var hrs = 0; var min = 0; 
+    do {
+
+        var horaEv = prompt('Ingrese la hora (formato: hh:mm 24hrs)');
+        hrs = Number( horaEv.substring(0,2));
+        min = Number( horaEv.substring(3,5));
+
+        //alert('Hora incorrecta')
+
+        if (hrs >= 25) {
+            alert('Ha ingresado una hora incorrecta');
+        }else{
+            
+            if (min >= 60) {
+            alert('Los minutos ingresados son incorrectos');
+            }else{
+
+            }
+        }
+    }while (hrs >= 25 || min >= 60);
+
+    
     
     // Repetir hasta que la fecha es correcta
     var d = 0; var m = 0; var a = 0;
-    while (a < 2022 || m > 12  && m <= 2  || d > 31) {
+
+    while (a < 2022 || a >= 2023 || a == 2022 && m == 1 || m == 2 || m > 12  && m <= 2  || d > 31) {
 
         var fechaEv = prompt('Ingrese la fecha (formato: dd/mm/aaaa)');
-                    //nombre cadena
-         d = Number( fechaEv.substring(0,2)); //Se define como numero, se pone el nombre de la subcadena y los espacios en los que se va a trabajar
+                   
+         d = Number( fechaEv.substring(0,2)); 
          m = Number( fechaEv.substring(3,5));
          a = Number( fechaEv.substring(6,10));
+         
         
         if (a < 2022 || m > 12  || d > 31) {
     
             alert('Usted ha ingresado una fecha incorrecta');
 
         } 
-        else if (a == 2022 && m == 1) {
+        else {
 
+            if(a == 2022 && m == 1) {
+    
             alert('Su fecha contiene el mes de Enero de 2022, es una fecha pasada');
+    
+            } else if (a == 2022 && m == 2) {
 
-        } 
-        else if (a == 2022 && m == 2) {
             alert('Su fecha contiene el mes de Febrero de 2022, es una fecha pasada');
-        }
-         else{
+
+            }else if (a >= 2023) {
+
+            alert('La fecha límite para contratos es 31/12/2022')
 
         }
+        }
+
+        
         //Para los meses que tienen 30 - 31 dias 
         switch (m){
     
@@ -61,8 +93,9 @@ function evento(){
         
                 default:     
         }
+        
     }
-
+    
     alert('¡Tu evento ha sido agregado con éxito!');
 
     document.getElementById('nombreEvento').innerHTML='' + nombreEv;
